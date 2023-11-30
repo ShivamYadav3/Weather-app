19acb0fd5c369bfeecf7816205185597683d547c
Shivam Yadav <shivamyadav93000@gmail.com>
2023-11-30 18:22:28 +0530

    update

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -60,10 +60,10 @@ const Weather = () => {
           {data.list.map((weatherData) => (
             <div
               key={weatherData.dt}
-              className="border p-4 rounded-xl bg-gray-200 w-96"
+              className="border p-4 rounded-xl bg-gray-200"
             >
-              <div className="min-h-screen flex items-center justify-center">
-                <div className="flex flex-col bg-white rounded gap-3">
+              <div className="flex items-center justify-center">
+                <div className="flex flex-col rounded gap-3">
                   <div className="font-bold">{data.city.name}</div>
                   <div className="flex gap-8 text-sm text-gray-500 ">
                     <div>{weatherData.dt_txt.split(" ")[0]}</div>
@@ -71,9 +71,7 @@ const Weather = () => {
                       {weatherData.dt_txt.split(" ")[1].split(":")[0]}:00
                     </div>
                   </div>
-                  {/* <div className="text-3xl inline-flex items-center justify-center rounded-lg text-indigo-400 h-24 w-24">
-                    
-                  </div> */}
+
                   <div className="flex justify-center items-center text-3xl">
                     <div>
                       {weatherData.weather[0].description
@@ -82,7 +80,7 @@ const Weather = () => {
                         weatherData.weather[0].description.slice(1)}
                     </div>
                   </div>
-                  <div className="flex gap-8">
+                  <div className="flex justify-center items-center gap-8">
                     <div className="font-medium text-3xl">
                       {(
                         Math.round((weatherData.main.temp - 273.15) * 100) / 100
