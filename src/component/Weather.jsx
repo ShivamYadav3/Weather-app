fff67781c335beab60121d28f76277107b290679
Shivam Yadav <shivamyadav93000@gmail.com>
2023-11-30 17:35:14 +0530

    css

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -11,7 +11,6 @@ const Weather = () => {
         `http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},IN&appid=${APIkey}`
       );
       const result = await response.json();
-      console.log(APIkey, result);
 
       setData(result);
     } catch (error) {
